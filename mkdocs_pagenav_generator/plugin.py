5e77a342d3affb60cdc2a209ac3d818902c466e5
Andre_601 <11576465+Andre601@users.noreply.github.com>
2021-02-23 00:33:24 +0100

    Create plugin.py

---
+++ mkdocs_pagenav_generator/plugin.py
@@ -0,0 +1,33 @@
+from mkdocs.plugins import BasePlugin
+from mkdocs.utils import normalize_url
+
+
+class InlineNavigationPlugin(BasePlugin):
+
+    def on_nav(self, nav, config, files):
+        self.nav = nav
+
+    def on_post_page(self, output, page, config):
+        if '{nav}' in output:
+            children = page.parent.children if page.parent else self.nav.items
+            siblings = [child for child in children if child != page]
+            return output.replace('{nav}', self._format_links(siblings, page, config))
+
+    def _format_links(self, items, page, config):
+        result = '<ul>'
+
+        for item in items:
+            result += '<li>'
+
+            if item.is_section:
+                result += item.title
+                result += self._format_links(item.children, page, config)
+            else:
+                url = normalize_url(item.url, page)
+                result += f'<a href="{url}">{item.title}</a>'
+
+            result += '</li>'
+
+        result += '</ul>'
+
+        return result
